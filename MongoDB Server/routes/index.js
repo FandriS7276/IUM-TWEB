7a78645ea032722b24cc821e3d6a6b66fa94aac4
Andrei Lucian Fistos <andrei.fistos@edu.unito.it>
2026-02-16 16:54:14 +0100

    Added '/api/ to all router.get calls in '/routes/index.js'

--- MongoDB Server/routes/index.js
+++ MongoDB Server/routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var oscarController = require('../controller/oscarController');
 var rotomController = require('../controller/rotomController');
 
-router.get('/oscar', oscarController.getAllOscars);
-router.get('/rotom', rotomController.getAllRottenReviews);
-router.get('/controversial-winners', oscarController.getControversialOscarWinners);
-router.get('/snubbed-movies', rotomController.getSnubbedMovies);
-router.get('/reviews-by-type', rotomController.getReviewsByType);
+router.get('/api/oscar', oscarController.getAllOscars);
+router.get('/api/rotom', rotomController.getAllRottenReviews);
+router.get('/api/controversial-winners', oscarController.getControversialOscarWinners);
+router.get('/api/snubbed-movies', rotomController.getSnubbedMovies);
+router.get('/api/reviews-by-type', rotomController.getReviewsByType);
