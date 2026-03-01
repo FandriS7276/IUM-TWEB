48a384343bef4e8bb76854c27f89d546843b381b
Andrei Lucian Fistos <andrei.fistos@edu.unito.it>
2026-03-01 13:15:14 +0100

    feat: add review creation endpoints with like and report functionality

--- MongoDB Server/routes/index.js
+++ MongoDB Server/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const oscarController = require('../controller/oscarController');
 const reviewReadController = require('../controller/reviewReadController');
+const reviewWriteController = require('../controller/reviewWriteController');
 
 // Oscar endpoints
 router.get('/awards/oscar', oscarController.getAllOscars);
@@ -13,4 +14,10 @@ router.get('/snubbed-movies', reviewReadController.getSnubbedMovies);
 router.get('/reviews-by-type', reviewReadController.getReviewsByType);
 router.get('/reviews/movie/:movieTitle', reviewReadController.getReviewsByMovie);
 router.get('/reviews/movie/:movieTitle/stats', reviewReadController.getMovieReviewStats);
+
+// Review creation endpoint
+router.post('/reviews', requireAuth, writeLimiter, validateReview, reviewWriteController.createReview);
+router.post('/reviews/:id/like', requireAuth, writeLimiter, reviewWriteController.likeReview);
+router.post('/reviews/:id/report', requireAuth, writeLimiter, reviewWriteController.reportReview);
+
 module.exports = router;
