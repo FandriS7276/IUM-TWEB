b808b2748b32be1a1e118a6ff2cca82faeff8d94
Andrei Lucian Fistos <andrei.fistos@edu.unito.it>
2026-03-01 12:05:24 +0100

    refractor: rename review reading and writing controllers to be more explicit and conventional

--- MongoDB Server/routes/index.js
+++ MongoDB Server/routes/index.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
 const oscarController = require('../controller/oscarController');
-const rottenController = require('../controller/rottenController');
+const reviewReadController = require('../controller/reviewReadController');
 
 // Oscar endpoints
 router.get('/awards/oscar', oscarController.getAllOscars);
 router.get('/awards/controversial-winners', oscarController.getControversialOscarWinners);
 
 // Rotten Tomatoes endpoints
-router.get('/reviews', rottenController.getAllReviews);
-router.get('/snubbed-movies', rottenController.getSnubbedMovies);
-router.get('/reviews-by-type', rottenController.getReviewsByType);
-router.get('/reviews/movie/:movieTitle', rottenController.getReviewsByMovie);
-router.get('/reviews/movie/:movieTitle/stats', rottenController.getMovieReviewStats);
+router.get('/reviews', reviewReadController.getAllReviews);
+router.get('/snubbed-movies', reviewReadController.getSnubbedMovies);
+router.get('/reviews-by-type', reviewReadController.getReviewsByType);
+router.get('/reviews/movie/:movieTitle', reviewReadController.getReviewsByMovie);
+router.get('/reviews/movie/:movieTitle/stats', reviewReadController.getMovieReviewStats);
 module.exports = router;
