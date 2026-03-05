0eed6cde7a96026ca7c7742e3d60dce2ae50361b
Andrei Lucian Fistos <andrei.fistos@edu.unito.it>
2026-03-05 17:35:46 +0100

    refactor: replace getAllStats with getMovieStats for enriching movie titles and improved the output mapping

--- MongoDB Server/utils/enrichment.js
+++ MongoDB Server/utils/enrichment.js
@@ -1,19 +1,18 @@
-const { getAllStats } = require('../database/statsCache');
+const { getMovieStats } = require('../services/statsCache');
 
 /**
  * Enriches array of movie titles with current stats + sorts by freshCount descending
  * @param {string[]} titles - array of movie titles
  * @returns {Array<Object>} enriched stats objects (sorted)
  */
-async function enrichAndSortByFreshness(titles) {
-    if (!titles?.length) return [];
-
-    const allStats = await getAllStats();
-
-    return titles
-        .map(title => allStats[title])
-        .filter(stat => stat !== null)           // skip movies without stats
-        .sort((a, b) => b.freshCount - a.freshCount); // most liked first
+async function enrichWithStats(titles) {
+    if (!titles.length) return [];
+    const stats = await Promise.all(titles.map(getMovieStats));
+    return titles.map((title, i) => ({
+        title,
+        stats: stats[i] || { freshCount: 0, tomatometer: 0, totalReviews: 0 }
+    }));
 }
 
-module.exports = { enrichAndSortByFreshness };
+
+module.exports = { enrichWithStats };
