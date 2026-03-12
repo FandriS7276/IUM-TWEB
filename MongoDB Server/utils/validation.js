d3818fa0dffcd9030db6162144911908befb7f13
Andrei Lucian Fistos <andrei.fistos@edu.unito.it>
2026-03-12 17:13:36 +0100

    refractor(API): remove needless value existence check in validation

--- MongoDB Server/utils/validation.js
+++ MongoDB Server/utils/validation.js
@@ -2,9 +2,6 @@ const VALID_CATEGORIES = require('../services/oscarCache').VALID_CATEGORIES || [
 
 // Validate if specified category exists
 function validateCategory(category) {
-    if (!category)
-        return null; // no filter = ok
-
     const normalized = category.trim().toUpperCase();
     
     if (!VALID_CATEGORIES.includes(normalized)) {
@@ -16,8 +13,6 @@ function validateCategory(category) {
 
 // Validate if a film exists
 async function isValidFilm(title) {
-    if (!title) return false;
-
     const normalized = title.trim().toLowerCase();
     return await client.sIsMember(ALL_MOVIES_KEY, normalized);
 }
