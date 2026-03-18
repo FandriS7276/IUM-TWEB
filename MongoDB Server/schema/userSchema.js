4f33e4adffd7ea7db7af1766923d1f2721fc2e4a
Andrei Lucian Fistos <andrei.fistos@edu.unito.it>
2026-03-18 15:57:50 +0100

    feat(API): implement password encryption on first save or modify

--- MongoDB Server/schema/userSchema.js
+++ MongoDB Server/schema/userSchema.js
@@ -27,18 +27,17 @@ const userSchema = new mongoose.Schema(
     }
 );
 
-userSchema.statics.updateCounter = async function(userId, field, delta) {
-    try {
-        await this.findByIdAndUpdate(
-        userId,
-        { $inc: { [field]: delta } },
-        { runValidators: true, new: true } // enforce schema rules
-        );
-    }
-    catch (err) {
-        console.error(`Failed to update user counter ${field} for user ${userId}:`, err);
-    }
-};
+// This function runs EVERY time .save() is called on a User document
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('passwordHash')) return next(); // <this> is the document about to be saved => returns true only if the document is new or was changed
+    const salt = await bcrypt.genSalt(12); // Encrypter - 12 to 24 is current professional standard (2026) and determines the computational cost for the generation of a random unique string
+    this.passwordHash = await bcrypt.hash(this.passwordHash, salt); // Combine actual password with salt and apply hashing algorithm => transforms a string of any length to a fixed length (digest)
+    next();
+});
 
+// Instance method - used in login to match inserted password with existing password in database
+userSchema.methods.verifyPassword = async function (passwordAttempt) {
+    return bcrypt.compare(passwordAttempt, this.passwordHash);
+};
 
 module.exports = mongoose.model('User', userSchema);
